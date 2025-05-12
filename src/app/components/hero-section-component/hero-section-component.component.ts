df299caed2846c51b8d5c6f29a7ca4391f5432f3
Nouhaila1937 <lahsaouinouhaila@gmail.com>
2025-05-12 21:01:11 +0100

    ajout de défilement vers le bas grace à l'icone de landing page

--- src/app/components/hero-section-component/hero-section-component.component.ts
+++ src/app/components/hero-section-component/hero-section-component.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { RouterLink } from '@angular/router';
+
 @Component({
   selector: 'app-hero-section-component',
   standalone: true, // Ajout de standalone: true
@@ -12,5 +13,15 @@ import { RouterLink } from '@angular/router';
   styleUrl: './hero-section-component.component.scss'
 })
 export class HeroSectionComponentComponent {
-
+scrollToFooter() {
+    // Recherche du footer dans le DOM par son tag HTML ou un ID spécifique
+    const footerElement = document.querySelector('footer');
+    
+    if (footerElement) {
+      // Défilement vers le footer avec animation fluide
+      footerElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  }
 }
